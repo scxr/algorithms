ea85268969ee814e62141c2eafe2af2f4d95078d
cswil <56652842+cswil@users.noreply.github.com>
2020-01-06 12:57:20 +0000

    Update insertionsort.go

--- sorting/insertionsort.go
+++ sorting/insertionsort.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+)
+
+var insertindex int
+
+func insertionsort(collection []int) []int {
+	for loopindex := 1; loopindex < len(collection); loopindex++ {
+		insertindex = loopindex
+		for ; insertindex > 0 && collection[insertindex-1] > collection[insertindex]; insertindex-- {
+			collection[insertindex] = collection[insertindex-1]
+			collection[insertindex-1] = collection[insertindex]
+
+		}
+	}
+	return collection
+}
+
+func main() {
+	unsorted := []int{3, 4, 1, 5, 2, 232,2,432,1} // values to be sorted
+	fmt.Println(insertionsort(unsorted))
+
+}
