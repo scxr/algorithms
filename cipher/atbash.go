5e896052aa0c05ec9d10b4d2d355e46528f63f5e
cswil <56652842+cswil@users.noreply.github.com>
2020-01-09 19:00:38 +0000

    Create atbash.go

---
+++ cipher/atbash.go
@@ -0,0 +1,27 @@
+/*
+Atbash is a substitution cipher where all the letters are revered. for example A->Z a->z b->y B->Y etc..
+*/
+package main
+
+import "fmt"
+
+func atbash(mystr string) string {
+
+	out := ""
+	for _, char := range mystr {
+		intchar := int(char)
+		if 65 <= intchar && intchar <= 90 { //lower case characters
+			out += string(155 - intchar) // reverse
+		} else if 97 <= intchar && intchar <= 122 { //uppercase characters
+			out += string(219 - intchar) // reverse
+		} else {
+			out += string(char) //for example numbers
+		}
+
+	}
+	return out
+}
+//test
+func main() {
+	fmt.Print(atbash("Test"))
+}
